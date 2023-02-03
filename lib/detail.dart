6c75b2f1092de07f3dfd09413ff3b381b34b7b5d
ShauryaPanwar <panwarshaurya11@gmail.com>
2023-02-03 16:49:51 +0530

    event list appended

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:event_details/utils/spaces.dart';
+import 'package:event_details/widgets/event_card.dart';
 import 'package:flutter/material.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class detail extends StatefulWidget {
   const detail({Key? key}) : super(key: key);
@@ -10,9 +13,7 @@ class detail extends StatefulWidget {
   State<detail> createState() => _detailState();
 }
 
-int _selectedindex=0;
-
-
+int _selectedindex = 0;
 
 class _detailState extends State<detail> {
   @override
@@ -21,59 +22,95 @@ class _detailState extends State<detail> {
       bottom: false,
       child: Scaffold(
         backgroundColor: Colors.white,
-        appBar:AppBar(
-          foregroundColor: Colors.white,
-          title: Text(" Event Details ",) ,
-          centerTitle: true,
-          toolbarHeight: 45,
-          toolbarOpacity: 1,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20))),
-          backgroundColor: Colors.deepPurple,
-          elevation: 9,
-          leading: IconButton(
-            color: Colors.white,
-            onPressed: () {},
-            icon: const Icon(LineAwesomeIcons.angle_left),
-          ),
-          actions: [
-
-          ],
-        ),
         body: SingleChildScrollView(
           child: Container(
+            height: MediaQuery.of(context).size.height * 1,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/dices.jpeg'), fit: BoxFit.cover)),
             padding: EdgeInsets.all(10),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                verticalspacing(11),
-
-
-              ],
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  verticalspacing(11),
+                  TextField(
+                    style: GoogleFonts.adventPro(fontWeight: FontWeight.w800),
+                    decoration: InputDecoration(
+                        icon: Icon(LineAwesomeIcons.search),
+                        hintText: "Search",
+                        contentPadding: EdgeInsets.all(5),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        )),
+                    // cursorColor: Colors.lightBlueAccent,
+                  ),
+                  verticalspacing(11),
+                  CardEvent(Date: "14 Feb", event_name: " Party Event", image: AssetImage('assets/drinks.png')),
+                  verticalspacing(11),
+                  CardEvent(Date: "23 May", event_name: "Birthday Event", image: AssetImage('assets/Party.png')),
+                  verticalspacing(11),
+                  Card(
+                    color: Colors.black12,
+                    clipBehavior: Clip.antiAlias,
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 5.0,sigmaY: 5.0),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.256,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [Colors.black.withOpacity(0.40), Colors.black.withOpacity(0.2),Colors.grey.withOpacity(0.01)],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight),
+                            borderRadius: BorderRadius.circular(5.0)),
+                        child: Material(
+                          child: InkWell(
+                            onTap: (){},
+                            child: Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text("18 March",
+                                      style: GoogleFonts.lato(
+                                          color: Colors.white, fontSize: 50)),
+                                  Text('Social Work',
+                                      style: GoogleFonts.lato(
+                                          color: Colors.white, fontSize: 20)),
+                                  TextButton(
+                                      onPressed: () {},
+                                      child: Center(
+                                        child: Text(
+                                          'Join Event',
+                                          style: TextStyle(color: Colors.white.withOpacity(0.8)),
+                                        ),
+                                      )
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 5.0),
+                  )
+                ]
             ),
           ),
         ),
-
-
-
-
-
         bottomNavigationBar: Container(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
-            )
-          ),
+              borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          )),
           child: ClipRRect(
             borderRadius: const BorderRadius.only(
               topRight: Radius.circular(20),
               topLeft: Radius.circular(20),
             ),
-
             child: BottomNavigationBar(
               type: BottomNavigationBarType.fixed,
               elevation: 9,
@@ -83,11 +120,10 @@ class _detailState extends State<detail> {
               unselectedItemColor: Colors.white.withOpacity(.60),
               selectedFontSize: 16,
               unselectedFontSize: 14,
-              onTap: (value){
+              onTap: (value) {
                 setState(() {
                   _selectedindex = value;
                 });
-
               },
               items: [
                 BottomNavigationBarItem(
@@ -109,7 +145,7 @@ class _detailState extends State<detail> {
               ],
             ),
           ),
-         ),
+        ),
       ),
     );
   }
---
+++ lib/widgets/event_card.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CardEvent extends StatelessWidget {
+  final String Date;
+  final String event_name;
+  final AssetImage image;
+
+  const CardEvent(
+      {Key? key,
+      required this.Date,
+      required this.event_name,
+        required this.image})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.black12,
+      clipBehavior: Clip.antiAlias,  
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0,sigmaY: 10.0),
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.256,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.black38, Colors.black12],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight),
+              borderRadius: BorderRadius.circular(5.0),
+              image: DecorationImage(
+                  colorFilter: ColorFilter.mode(Colors.grey, BlendMode.saturation),
+                  fit: BoxFit.cover,
+                  image: image)),
+          child: Material(
+            child: InkWell(
+              onTap: (){},
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40),
+                child: Column(
+                  children: [
+                    Text("$Date",
+                        style: GoogleFonts.lato(
+                            color: Colors.white, fontSize: 50)),
+                    Text('$event_name',
+                        style: GoogleFonts.lato(
+                            color: Colors.white, fontSize: 20)),
+                    TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Join Event',
+                          style: TextStyle(color: Colors.white),
+                        ))
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 5.0),
+    );
+  }
+}
